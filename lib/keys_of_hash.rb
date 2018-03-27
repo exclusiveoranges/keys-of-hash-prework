a9e401414390e5436fbdb6dad8dfd4d6bec04fa2
Ryan O'Toole <otoolerj@gmail.com>
2018-03-27 03:32:57 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,13 +4,10 @@ require 'pry'
 class Hash
   def keys_of(*arguments)
     # binding.pry
-    self.keys
+    # self.keys
     self.each do |key, value|
       if value == arguments
         return value
-        
-      binding.pry
+      end
     end
-
-  end
 end
